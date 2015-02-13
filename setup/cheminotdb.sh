0611b76b06441bab3730ad54ae8973caf484f7a4
Sebastien Renault <sre@zenexity.com>
2015-02-13 09:25:53 +0100

    Update cheminotdb script.

--- setup/cheminotdb.sh
+++ setup/cheminotdb.sh
@@ -4,8 +4,13 @@ echo "\n---------------------"
 echo "Installing DB"
 echo "---------------------"
 
-cp "../cheminot.db/db/current/cheminot.db" app/platforms/android/assets/
-cp "../cheminot.db/db/current/calendar_dates" app/platforms/android/assets/
-cp "../cheminot.db/db/current/graph" app/platforms/android/assets/
+rm app/platforms/android/assets/calendar_dates*
+rm app/platforms/android/assets/cheminot*
+rm app/platforms/android/assets/stops_ttree*
+rm app/platforms/android/assets/graph*
+
+cp ../cheminot.db/db/current/* app/platforms/android/assets/
+
+rm app/platforms/android/assets/stops_ttree*
 
 echo "\nDONE!"
