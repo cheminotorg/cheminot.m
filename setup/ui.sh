8a01985878e1f6f3e4cf4dc3451c3af6f9008766
Sebastien Renault <sre@zenexity.com>
2015-02-25 14:49:14 +0100

    Cheminot icons.

--- setup/ui.sh
+++ setup/ui.sh
@@ -2,4 +2,5 @@
 
 rsync -av ../cheminot.ui/dist/fonts/target/ project/www/fonts/cheminot/
 rsync -av ../cheminot.ui/dist/splashscreens/ images/android/default/splashscreens/
+rsync -av ../cheminot.ui/dist/icons/ images/android/default/icons/
 cp ../cheminot.ui/dist/loader/*.gif project/www/images/
