3b3b3548d2e057e350c39a167ed49cc1432ef835
Sebastien Renault <sre@zenexity.com>
2015-02-25 11:17:13 +0100

    Update splashscreens (nine patch).

--- setup/ui.sh
+++ setup/ui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash$
 
 rsync -av ../cheminot.ui/dist/fonts/target/ project/www/fonts/cheminot/
+rsync -av ../cheminot.ui/dist/splashscreens/ images/android/default/splashscreens/
 cp ../cheminot.ui/dist/loader/*.gif project/www/images/
