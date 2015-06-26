1414366ea866f938ea3c277b33b45fea87629471
Sebastien Renault <srenault.contact@gmail.com>
2015-06-26 11:08:31 +0200

    Upload git version to dropbox.

--- setup/demo.sh
+++ setup/demo.sh
@@ -5,3 +5,6 @@ cd app/platforms/browser/
 zip -r cheminotm.zip www/
 dropbox_uploader.sh -p upload cheminotm.zip cheminotm-latest.zip
 dropbox_uploader.sh share cheminotm-latest.zip
+rm version
+git describe --long --always > version
+dropbox_uploader.sh -p upload version cheminotm_version
