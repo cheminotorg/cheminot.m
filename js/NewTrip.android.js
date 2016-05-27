cb8405761e121dfa84e1b59abc5f40f0ce1fb4d5
Sebastien Renault <srenault.contact@gmail.com>
2016-05-27 09:18:09 +0200

    Animate inputs

--- js/NewTrip.android.js
+++ js/NewTrip.android.js
@@ -9,13 +9,19 @@ import React, {
   DatePickerAndroid,
   TimePickerAndroid,
   TouchableWithoutFeedback,
-  Animated
+  Animated,
+  Easing,
+  Dimensions,
+  ListView,
+  RecyclerViewBackedScrollView,
+  TouchableNativeFeedback
 } from 'react-native';
 
 import DrawerContainer from './layout/DrawerContainer';
 import NavigationBackAndroidContainer from './layout/NavigationBackAndroidContainer';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { MKColor, MKTextField, MKButton } from 'react-native-material-kit';
+import Cheminotdb from './Cheminotdb';
 
 const SearchButton = MKButton.flatButton()
                              .withBackgroundColor(MKColor.Teal)
@@ -31,27 +37,28 @@ const {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 56 + 20,
+    marginTop: 56 + 20
   },
   block: {
     backgroundColor: 'white',
     marginBottom: 20,
-    borderColor: '#ffffff',
-    borderWidth: 1,
-    shadowColor: MKColor.Grey,
-    shadowOpacity: 0.8,
-    shadowOffset: {
-      height: 1,
-      width: 2
-    }
+    elevation: 2
   },
   tripBlock: {
+    top: 0,
     paddingTop: 12,
     paddingBottom: 24,
     paddingLeft: 16,
     paddingRight: 16
   },
-  datetimeBlock: {
+  suggestionBlock: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    paddingTop: 4,
+    paddingLeft: 10,
+    paddingRight: 10
   },
   datetimeRow: {
     flex: 1,
@@ -81,21 +88,15 @@ const styles = StyleSheet.create({
 class NewTrip extends Component {
 
   state = {
-    departure: {
-      top: new Animated.Value(10)
-    },
-    arrival: {
-      top: new Animated.Value(40)
-    }
-  }
-
-  componentDidMount() {
-    /*Animated.timing(
-     *  this.state.departure.top, {
-     *    toValue: 1000,
-     *    duration: 2000
-     *  }
-     *).start();*/
+    stations: [],
+    isFocusDeparture: false,
+    isFocusArrival: false,
+    selectedDeparture: {},
+    selectedArrival: {},
+    tripBlockTop: new Animated.Value(0),
+    tripBlockHeight: new Animated.Value(116),
+    datetimeBlockTop: new Animated.Value(0),
+    suggestionBlockTop: new Animated.Value(Dimensions.get('window').height)
   }
 
   componentWillMount() {
@@ -112,41 +113,263 @@ class NewTrip extends Component {
     TimePickerAndroid.open({});
   }
 
+  onDepartureFocus() {
+    this.setState({
+      isFocusDeparture: true
+    });
+    focusDepartureInput(this.state);
+  }
+
+  onArrivalFocus() {
+    this.setState({
+      isFocusArrival: true
+    });
+    focusArrivalInput({
+      tripBlockTop: this.state.tripBlockTop,
+      datetimeBlockTop: this.state.datetimeBlockTop,
+      suggestionBlockTop: this.state.suggestionBlockTop
+    });
+  }
+
+  onDepartureChangeText(term) {
+    Cheminotdb.searchStops(term, 50).then((stations) => {
+      this.setState({
+        stations: stations
+      });
+    });
+  }
+
+  onArrivalChangeText(term) {
+    Cheminotdb.searchStops(term, 50).then((stations) => {
+      this.setState({
+        stations: stations
+      });
+    });
+  }
+
+  onStationSelected(id, name) {
+    if(this.state.isFocusDeparture) {
+      this.onDepartureSelected(id, name);
+    } else {
+      this.onArrivalSelected(id, name);
+    }
+  }
+
+  onDepartureSelected(id, name) {
+    this.setState({
+      isFocusDeparture: false,
+      selectedDeparture: {
+        id: id,
+        name: name
+      }
+    });
+    blurDepartureInput(this.state);
+  }
+
+  onArrivalSelected(id, name) {
+    this.setState({
+      isFocusArrival: false,
+      selectedArrival: {
+        id: id,
+        name: name
+      }
+    });
+    blurArrivalInput(this.state);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.block, styles.tripBlock]}>
+        <Animated.View style={[styles.block, styles.tripBlock, {top: this.state.tripBlockTop, height: this.state.tripBlockHeight}]}>
           <Animated.View>
-            <MKTextField placeholder="Départ" style={[styles.textfield, {marginBottom: 14}]} />
+            <MKTextField
+               value={this.state.selectedDeparture.name || ''}
+               onChangeText={this.onDepartureChangeText.bind(this)}
+               onFocus={this.onDepartureFocus.bind(this)}
+               placeholder="Départ"
+               style={[styles.textfield, {marginBottom: 14}]} />
           </Animated.View>
-          <Animated.View>
-            <MKTextField placeholder="Arrivée" style={styles.textfield} />
+          <Animated.View style={{top: this.state.arrivalInputTop}}>
+            <MKTextField
+               value={this.state.selectedArrival.name || ''}
+               onChangeText={this.onArrivalChangeText.bind(this)}
+               onFocus={this.onArrivalFocus.bind(this)}
+               placeholder="Arrivée"
+               style={styles.textfield} />
           </Animated.View>
         </Animated.View>
-        <View style={[styles.block, styles.datetimeBlock]}>
-          <View style={styles.datetimeRow}>
-            <TouchableWithoutFeedback onPress={this.onDatePickerPress.bind(this)}>
-              <View style={{flexDirection: 'row'}}>
-                <Text style={styles.datetimeLabel}>Date de départ</Text>
-                <Text style={styles.datetimeValue}>21/01/2016</Text>
-              </View>
-            </TouchableWithoutFeedback>
+        <Animated.View style={{top: this.state.datetimeBlockTop}}>
+          <View style={styles.block}>
+            <View style={styles.datetimeRow}>
+              <TouchableWithoutFeedback onPress={this.onDatePickerPress.bind(this)}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={styles.datetimeLabel}>Date de départ</Text>
+                  <Text style={styles.datetimeValue}>21/01/2016</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+            <View style={[styles.dateRow, styles.datetimeRow]}>
+              <TouchableWithoutFeedback style={{flex: 1, backgroundColor: 'green'}} onPress={this.onTimePickerPress.bind(this)}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{flex: 1}}>Heure de départ</Text>
+                  <Text style={styles.datetimeValue}>08:00</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
           </View>
-          <View style={[styles.dateRow, styles.datetimeRow]}>
-            <TouchableWithoutFeedback style={{flex: 1, backgroundColor: 'green'}} onPress={this.onTimePickerPress.bind(this)}>
-              <View style={{flexDirection: 'row'}}>
-                <Text style={{flex: 1}}>Heure de départ</Text>
-                <Text style={styles.datetimeValue}>08:00</Text>
-              </View>
-            </TouchableWithoutFeedback>
+          <View style={styles.submitBlock}>
+            <SearchButton />
           </View>
-        </View>
-        <View style={styles.submitBlock}>
-          <SearchButton />
-        </View>
+        </Animated.View>
+        <Animated.View style={[styles.suggestionBlock, {top: this.state.suggestionBlockTop, backgroundColor: 'white'}]}>
+          <StationsList
+             stations={this.state.stations || []}
+             style={{backgroundColor: 'white'}}
+             onItemSelected={this.onStationSelected.bind(this)} />
+        </Animated.View>
       </View>
     );
   }
 }
+
+function animateArrivalInput(from, to) {
+  const {height, width} = Dimensions.get('window');
+
+  Animated.parallel([
+    Animated.timing(
+      from.tripBlockTop, {
+        easing: Easing.inOut(Easing.quad),
+        toValue: to.tripBlockTop,
+        duration: 250
+      }
+    ),
+    Animated.timing(
+      from.datetimeBlockTop, {
+        easing: Easing.ease,
+        toValue: to.datetimeBlockTop,
+        duration: 250
+      }
+    ),
+    Animated.timing(
+      from.suggestionBlockTop, {
+        easing: Easing.out(Easing.ease),
+        toValue: to.suggestionBlockTop,
+        duration: 250
+      }
+    )
+  ]).start();
+}
+
+function focusArrivalInput(from) {
+  animateArrivalInput(from, {
+    tripBlockTop: -65,
+    datetimeBlockTop: Dimensions.get('window').height,
+    suggestionBlockTop: 46
+  });
+}
+
+function blurArrivalInput(from) {
+  animateArrivalInput(from, {
+    tripBlockTop: 0,
+    datetimeBlockTop: 0,
+    suggestionBlockTop: Dimensions.get('window').height
+  });
+}
+
+function animateDepartureInput(from, to) {
+  const {height, width} = Dimensions.get('window');
+
+  Animated.parallel([
+    Animated.timing(
+      from.tripBlockTop, {
+        easing: Easing.inOut(Easing.quad),
+        toValue: to.tripBlockTop,
+        duration: 250
+      }
+    ),
+    Animated.timing(
+      from.tripBlockHeight, {
+        easing: Easing.ease,
+        toValue: to.tripBlockHeight,
+        duration: 250
+      }
+    ),
+    Animated.timing(
+      from.datetimeBlockTop, {
+        easing: Easing.ease,
+        toValue: to.datetimeBlockTop,
+        duration: 250
+      }
+    ),
+    Animated.timing(
+      from.suggestionBlockTop, {
+        easing: Easing.out(Easing.ease),
+        toValue: to.suggestionBlockTop,
+        duration: 250
+      }
+    )
+  ]).start();
+}
+
+function focusDepartureInput(from) {
+  animateDepartureInput(from, {
+    tripBlockTop: -20,
+    tripBlockHeight: 58,
+    datetimeBlockTop: Dimensions.get('window').height,
+    suggestionBlockTop: 46
+  });
+}
+
+function blurDepartureInput(from) {
+  animateDepartureInput(from, {
+    tripBlockTop: 0,
+    tripBlockHeight: 58 * 2,
+    datetimeBlockTop: 0,
+    suggestionBlockTop: Dimensions.get('window').height
+  });
+}
+
+const StationsList = React.createClass({
+
+  getInitialState: function() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (station1, station2) => station1.id !== station2.id
+    });
+    return {
+      dataSource: ds.cloneWithRows(this.props.stations)
+    };
+  },
+
+  componentWillReceiveProps: function(props) {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(props.stations)
+    });
+  },
+
+  renderRow: function({id, name}) {
+    return (
+      <TouchableNativeFeedback id={id} onPress={this.props.onItemSelected.bind(this, id, name)}>
+        <View style={{paddingTop: 10, paddingBottom: 10, flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+          <Icon name="location-on" size={24} color={MKColor.Grey} />
+          <Text style={{color: MKColor.Grey}}>{name}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    )
+  },
+
+  render: function() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        enableEmptySections={true}
+        automaticallyAdjustContentInsets={true}
+        renderRow={this.renderRow}
+        renderScrollComponent={props => <RecyclerViewBackedScrollView {...props}/> }
+        renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={{backgroundColor: MKColor.Grey, height: 1}} />}
+        {...this.props}
+      />
+    );
+  }
+});
 
 module.exports = NavigationBackAndroidContainer.create(DrawerContainer.create(NavigationContainer.create(NewTrip)));
